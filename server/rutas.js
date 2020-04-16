52676de6e507cf3a3fff9ab0d36a640164a80622
Matti <mattibylin@Mattis-Air.ZyXEL-USG>
2020-04-16 11:10:34 -0500

    formamos la arq de server/rutas.js

--- server/rutas.js
+++ server/rutas.js
@@ -3,3 +3,18 @@ const Router=require('express').Router();
 Router.get('all', function(err, res){
   // Devolver todos los usuarios del servidor.
 })
+Router.get('/cargar_eventos', function(req, res){
+  //retorna eventos guardados.
+})
+Router.post('/login', function(req, res){
+
+})
+Router.post('/agregar_evento', function(req, res){
+
+})
+Router.post('/eliminar_evento:id', function(req, res){
+
+})
+Router.put('/actualizar_evento:id', function(req, res){
+
+})
